65623c57cfaff9d062704d1b58f93624dfe22696
EltonPajaziti <pajazitielton002@gmail.com>
2024-12-27 21:32:32 +0100

    added favicon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gjeje Mjeshtrin</title>
+    <link rel="icon" type="image/png" href="Images/FINAL_LOGO.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="CSS/header.css" rel="stylesheet">
 
